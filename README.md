a1915cfad49b2cc5776928d0978e98e6e0d3674c
EiffL <fr.eiffel@gmail.com>
2019-10-19 20:59:13 -0700

    Adds decent setup

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # GalFlow
 A TensorFlow reimplementation of GalSim
+
+This repository is meant to host TensorFlow implementations of useful GalSim
+functions.
+
+Features:
+  - FFT convolutions with x interpolant and Fourier wrapping.
+
+**Contributions and Feature Requests Welcome !!!**
+
+## Setup
+
+```python
+$ pip install --user galflow
+```
+
+For usage example, see [this notebook](notebooks/GalFlow_demo)
--- setup.py
+++ setup.py
@@ -4,11 +4,32 @@ import sys
 
 from setuptools import find_packages
 from setuptools import setup
+from io import open
+
+# read the contents of the README file
+with open('README.md', encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name='GalFlow',
     description='GalSim reimplementation in TensorFlow',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='ML4Astro Contributors',
+    url='http://github.com/ml4astro/GalFlow',
     license='MIT',
     packages=find_packages(),
-    zip_safe=False,
+    install_requires=[
+        'tensorflow<2.0.0',
+        'galsim'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Astronomy'
+    ],
+    keywords='astronomy',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm']
 )
