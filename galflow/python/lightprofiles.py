e800a8e2645c19206fca6aea3568a8fbc84a9961
b-remy <remy.benjamin.101@gmail.com>
2021-09-07 15:12:49 +0200

    update coding style

--- galflow/python/lightprofiles.py
+++ galflow/python/lightprofiles.py
@@ -11,12 +11,14 @@ import math
 
 from tensorflow.python.types.core import Value
 
+__all__ = ["gaussian", "sersic"]
+
 # The FWHM of a Gaussian is 2 sqrt(2 ln2) sigma
 fwhm_factor = 2*math.sqrt(2*math.log(2))
 # The half-light-radius is sqrt(2 ln2) sigma
 hlr_factor = math.sqrt(2*math.log(2))
 
-def gaussian(fwhm=None, half_light_radius=None, sigma=None, flux=1., nx=None, ny=None):
+def gaussian(fwhm=None, half_light_radius=None, sigma=None, flux=1., nx=None, ny=None, name=None):
   """Function for generating a Gaussian profile:
 
     :math:`I(r) = \exp\left(-\frac{r^2}{2\sigma^2}\right) / (2 \pi \sigma^2)`
@@ -35,38 +37,40 @@ def gaussian(fwhm=None, half_light_radius=None, sigma=None, flux=1., nx=None, ny
   Example:
     >>> gaussian(sigma=3., nx=55)
   """
-  if nx is None:
-    if ny is None:
-      raise ValueError("Either nx or ny or both must be specified")
-    else:
-      nx = ny
-  else:
-    if ny is None:
-      ny = nx
 
-  if fwhm is not None:
-    if half_light_radius is not None or sigma is not None:
-      raise ValueError("Only one of sigma, fwhm, and half_light_radius may be specified,\
-        fwhm={}, half_light_radius={}, sigma={}".format(fwhm, half_light_radius, sigma))
+  with tf.name_scope(name or "gaussian"):
+    if nx is None:
+      if ny is None:
+        raise ValueError("Either nx or ny or both must be specified")
+      else:
+        nx = ny
     else:
-      sigma = fwhm / fwhm_factor
-  elif half_light_radius is not None:
-    if sigma is not None:
-      raise ValueError("Only one of sigma, fwhm, and half_light_radius may be specified,\
-        fwhm={}, half_light_radius={}, sigma={}".format(fwhm, half_light_radius, sigma))
-    else:
-      sigma = half_light_radius / hlr_factor
-  elif sigma is None:
-    raise ValueError("One of sigma, fwhm, and half_light_radius must be specified,\
-        fwhm={}, half_light_radius={}, sigma={}".format(fwhm, half_light_radius, sigma))
-    
-  x, y = tf.cast(tf.meshgrid(tf.range(nx), tf.range(ny)), tf.float32)
-  z = tf.sqrt(tf.cast((x+.5-nx/2)**2 + (y+.5-ny/2)**2, tf.float32))
-  gaussian = flux * tf.exp(-z*z / 2 / sigma/sigma) / 2 / math.pi / sigma / sigma
+      if ny is None:
+        ny = nx
 
-  return gaussian
+    if fwhm is not None:
+      if half_light_radius is not None or sigma is not None:
+        raise ValueError("Only one of sigma, fwhm, and half_light_radius may be specified,\
+          fwhm={}, half_light_radius={}, sigma={}".format(fwhm, half_light_radius, sigma))
+      else:
+        sigma = fwhm / fwhm_factor
+    elif half_light_radius is not None:
+      if sigma is not None:
+        raise ValueError("Only one of sigma, fwhm, and half_light_radius may be specified,\
+          fwhm={}, half_light_radius={}, sigma={}".format(fwhm, half_light_radius, sigma))
+      else:
+        sigma = half_light_radius / hlr_factor
+    elif sigma is None:
+      raise ValueError("One of sigma, fwhm, and half_light_radius must be specified,\
+          fwhm={}, half_light_radius={}, sigma={}".format(fwhm, half_light_radius, sigma))
+      
+    x, y = tf.cast(tf.meshgrid(tf.range(nx), tf.range(ny)), tf.float32)
+    z = tf.sqrt(tf.cast((x+.5-nx/2)**2 + (y+.5-ny/2)**2, tf.float32))
+    gaussian = flux * tf.exp(-z*z / 2 / sigma/sigma) / 2 / math.pi / sigma / sigma
 
-def sersic(n, half_light_radius=None, scale_radius=None, flux=1., trunc=0., flux_untruncated=False, nx=None, ny=None):
+    return gaussian
+
+def sersic(n, half_light_radius=None, scale_radius=None, flux=1., trunc=0., flux_untruncated=False, nx=None, ny=None, name=None):
   """Function for generating a Sersic profile:
 
     :math:`I(r) = I0 \exp(-\left(r/r_0\right)^{\frac{1}{n}})`
@@ -89,48 +93,49 @@ def sersic(n, half_light_radius=None, scale_radius=None, flux=1., trunc=0., flux
   Example:
     >>> sersic(n=2, scale_radius=5., flux=40., nx=55)
   """
-  if nx is None:
-    if ny is None:
-      raise ValueError("Either nx or ny or both must be specified")
+  with tf.name_scope(name or "sersic"):
+    if nx is None:
+      if ny is None:
+        raise ValueError("Either nx or ny or both must be specified")
+      else:
+        nx = ny
     else:
-      nx = ny
-  else:
-    if ny is None:
-      ny = nx
+      if ny is None:
+        ny = nx
 
-  if trunc < 0.:
-    raise ValueError("Sersic trunc must be > 0.")
+    if trunc < 0.:
+      raise ValueError("Sersic trunc must be > 0.")
 
-  if half_light_radius is not None:
-    if scale_radius is not None:
-      raise ValueError("Only one of scale_radius and half_light_radius may be specified,\
-        scale_radius={}, half_light_radius={}".format(scale_radius, half_light_radius))
+    if half_light_radius is not None:
+      if scale_radius is not None:
+        raise ValueError("Only one of scale_radius and half_light_radius may be specified,\
+          scale_radius={}, half_light_radius={}".format(scale_radius, half_light_radius))
+      else:
+        raise NotImplementedError("to implement, see\
+          https://github.com/GalSim-developers/GalSim/blob/6c1eb247df86ec03d1a2c9c8024fe57b1bd4a154/src/SBSersic.cpp#L656")
+    elif scale_radius is not None:
+      r0 = scale_radius
     else:
-      raise NotImplementedError("to implement, see\
-        https://github.com/GalSim-developers/GalSim/blob/6c1eb247df86ec03d1a2c9c8024fe57b1bd4a154/src/SBSersic.cpp#L656")
-  elif scale_radius is not None:
-    r0 = scale_radius
-  else:
-    raise ValueError("Either scale_radius or half_light_radius must be specified for Sersic,\
-      half_light_radius={}, scale_radius={}".format(half_light_radius, scale_radius))
-    
-  if trunc > 0.:
-    flux_fraction = integratedflux(trunc, r0, n)
-    if not flux_untruncated:
-      flux /= flux_fraction
-  else:
-    flux_fraction = 1.
+      raise ValueError("Either scale_radius or half_light_radius must be specified for Sersic,\
+        half_light_radius={}, scale_radius={}".format(half_light_radius, scale_radius))
+      
+    if trunc > 0.:
+      flux_fraction = integratedflux(trunc, r0, n)
+      if not flux_untruncated:
+        flux /= flux_fraction
+    else:
+      flux_fraction = 1.
 
-  x, y = tf.cast(tf.meshgrid(tf.range(nx), tf.range(ny)), tf.float32)
-  z = tf.sqrt(tf.cast((x+.5-nx/2)**2 + (y+.5-ny/2)**2, tf.float32))
+    x, y = tf.cast(tf.meshgrid(tf.range(nx), tf.range(ny)), tf.float32)
+    z = tf.sqrt(tf.cast((x+.5-nx/2)**2 + (y+.5-ny/2)**2, tf.float32))
 
-  sersic = tf.exp(-tf.math.pow(z/r0, 1/n))
-  if trunc > 0.:
-    sersic = tf.cast((z<trunc), tf.float32) * sersic
-  sersic /= math.pi * r0 * r0 * math.factorial(2.*n)
-  sersic *= flux
+    sersic = tf.exp(-tf.math.pow(z/r0, 1/n))
+    if trunc > 0.:
+      sersic = tf.cast((z<trunc), tf.float32) * sersic
+    sersic /= math.pi * r0 * r0 * math.factorial(2.*n)
+    sersic *= flux
 
-  return sersic
+    return sersic
 
 def integratedflux(trunc, r0, n):
   """ Convenience function to compute the fraction of the total flux enclosed within a given radius
--- galflow/python/shear.py
+++ galflow/python/shear.py
@@ -12,9 +12,8 @@ from galflow.python.transform import transform
 
 __all__ = ["shear", "shear_transformation"]
 
-def shear_transformation(g1, g2, Fourier=False):
-  """
-  Function to compute the affine transformation corresponding to a given shear.
+def shear_transformation(g1, g2, Fourier=False, name=None):
+  """Function to compute the affine transformation corresponding to a given shear.
   This function uses the reduced shear definition:
 
     :math:`|g| = \frac{a - b}{a + b}`
@@ -30,34 +29,35 @@ def shear_transformation(g1, g2, Fourier=False):
       S = \frac{1}{\sqrt{1-g^2}}
               \left( \begin{array}{cc} 1+g_1 & g_2 \\
                                        g_2 & 1-g_1 \end{array} \right)
-  Parameters
-  ----------
-    g1: Tensor
-      The first component of the shear in the "reduced shear" definition.
-    g2: Tensor
-     The second component of the shear in the "reduced shear" definition.
-
-  Returns
-  -------
-    Transformation matrix format, i.e [(batch), 3, 3]
+  Args: 
+    g1: `Tensor`, The first component of the shear in the "reduced shear" definition.
+    g2: `Tensor`, The second component of the shear in the "reduced shear" definition.
+    Fourier: `boolean`, when doing an interpolation in Fourier space, the center of pixels is on
+    integer values, so set to true if transforming in Fourier space.
+    name: `string`, name of the operation.
+    
+  Returns:
+    `Tensor` of the transformation matrix of shape [(batch), 3, 3]
   """
-  g1 = tf.convert_to_tensor(g1, dtype=tf.float32)
-  g2 = tf.convert_to_tensor(g2, dtype=tf.float32)
-  gsqr = g1**2 + g2**2
 
-  # Building a batched jacobian
-  jac = tf.stack([ 1. + g1, g2,
-                g2, 1. - g1], axis=1) / tf.expand_dims(tf.sqrt(1.- gsqr),1)
-  jac = tf.reshape(jac, [-1,2,2])
+  with tf.name_scope(name or "shear_transformation"):
+    g1 = tf.convert_to_tensor(g1, dtype=tf.float32)
+    g2 = tf.convert_to_tensor(g2, dtype=tf.float32)
+    gsqr = g1**2 + g2**2
 
-  # Inverting these jacobians to follow the TF definition
-  if Fourier:
-    jac = tf.transpose(jac, [0,2,1])
-  else:
-    jac = tf.linalg.inv(jac)
-  jac = tf.pad(jac, tf.constant([[0, 0], [0, 1],[0,1]]) )
-  jac = jac + tf.pad(tf.reshape(tf.ones_like(g1), [-1,1,1]), tf.constant([[0,0],[2,0],[2,0]]))
-  return jac
+    # Building a batched jacobian
+    jac = tf.stack([ 1. + g1, g2,
+                  g2, 1. - g1], axis=1) / tf.expand_dims(tf.sqrt(1.- gsqr),1)
+    jac = tf.reshape(jac, [-1,2,2])
+
+    # Inverting these jacobians to follow the TF definition
+    if Fourier:
+      jac = tf.transpose(jac, [0,2,1])
+    else:
+      jac = tf.linalg.inv(jac)
+    jac = tf.pad(jac, tf.constant([[0, 0], [0, 1],[0,1]]) )
+    jac = jac + tf.pad(tf.reshape(tf.ones_like(g1), [-1,1,1]), tf.constant([[0,0],[2,0],[2,0]]))
+    return jac
 
 def shear(img, g1, g2):
   """ Convenience function to apply a shear to an input image or kimage.
